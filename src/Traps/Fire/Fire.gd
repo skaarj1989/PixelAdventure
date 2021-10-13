23ab1998418a7d0e299338c6d4c4baf2be84b0c4
David <skaarj1989@gmail.com>
2021-10-13 16:11:04 +0200

    Added SFX

--- src/Traps/Fire/Fire.gd
+++ src/Traps/Fire/Fire.gd
@@ -8,6 +8,10 @@ export(float) var burning_time := 1.0 # In seconds
 var state := OFF
 
 
+onready var match_being_struck_sfx := preload("res://assets/SFX/MatchBeingStruck.wav")
+onready var burning_sfx := preload("res://assets/SFX/Burning.wav")
+
+
 func _on_AnimatedSprite_animation_finished() -> void:
 	if $AnimatedSprite.animation == "hit":
 		$Timer.start(ignition_delay)
@@ -16,11 +20,9 @@ func _on_AnimatedSprite_animation_finished() -> void:
 func _on_Trigger_body_entered(body) -> void:
 	if body.is_in_group("player"): 
 		$AnimatedSprite.play("hit")
-		call_deferred("disable_trigger", true)
-
-
-func disable_trigger(disabled: bool) -> void:
-	$Trigger/CollisionShape2D.disabled = disabled
+		$Trigger/CollisionShape2D.set_deferred("disabled", true)
+		$Audio.stream = match_being_struck_sfx
+		$Audio.play()
 
 
 func _on_SearArea_body_entered(body) -> void:
@@ -29,10 +31,10 @@ func _on_SearArea_body_entered(body) -> void:
 
 
 func _on_Timer_timeout() -> void:
-	change_state(ON if state == OFF else OFF)
+	_change_state(ON if state == OFF else OFF)
 
 
-func change_state(new_state) -> void:
+func _change_state(new_state) -> void:
 	if new_state == state:
 		return
 	
@@ -40,10 +42,13 @@ func change_state(new_state) -> void:
 		ON:
 			$AnimatedSprite.play("on")
 			$SearArea/CollisionShape2D.disabled = false
+			$Audio.stream = burning_sfx
+			$Audio.play()
 			$Timer.start(burning_time)
 		OFF:
 			$AnimatedSprite.play("off")
+			$Trigger/CollisionShape2D.disabled = false
 			$SearArea/CollisionShape2D.disabled = true
-			disable_trigger(false)
+			$Audio.stop()
 		
 	state = new_state
